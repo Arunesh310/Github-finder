43f7b80cabbb73bcbb92de3ae97ca8fa661f1c6e
Nirbhay-sys <119494283+Nirbhay-sys@users.noreply.github.com>
2024-04-11 17:50:50 +0530

    bug fixes and changes

--- app.js
+++ app.js
@@ -6,43 +6,34 @@ const ui = new UI();
 
 const searchInput = document.querySelector("#search-input");
 const searchButton = document.querySelector("#search-btn");
-const alertDiv = document.querySelector(".alert");
 
 searchButton.addEventListener("click", getInput);
 
 function getInput() {
-  
   if (searchInput.value) {
-    
     github
       .fetchUserData(searchInput.value)
       .then((res) => {
-        
         if (res.data.message === "Not Found") {
           showAlert("User Not Found!");
         } else {
-          
           ui.renderProfile(res.data);
           ui.renderProjects(res.repos);
         }
       })
       .catch((err) => console.log(err));
-
     return;
   }
   showAlert("Please enter a username!");
 }
 
 function showAlert(message) {
+  const alertDiv = document.getElementById("alertDiv");
   alertDiv.textContent = message;
-  alertDiv.style.display = "block";
-  alertDiv.style.border = "2px solid #FFF3CD";
-  alertDiv.style.marginTop = "20px";
-  alertDiv.style.backgroundColor = "#FFF3CD";
-  alertDiv.style.color = "red";
+  alertDiv.classList.remove("d-none");
 
   setTimeout(() => {
-    alertDiv.style.display = "none";
+    alertDiv.classList.add("d-none"); // Hide the alert after 3 seconds
   }, 3000);
 }
 
@@ -51,7 +42,6 @@ const lightModeStylesheet = document.getElementById("light-mode-stylesheet");
 
 function toggleMode() {
   lightModeStylesheet.disabled = !lightModeStylesheet.disabled;
-
   const currentMode = lightModeStylesheet.disabled ? "Light Mode" : "Dark Mode";
   modeToggleBtn.innerText = currentMode;
 }
